3b0873a1e3ac5f9990e6e1342f85443460a5a033
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-19 20:45:57 +0545

    Added some comments

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ h1.style.backgroundColor = "black";
 const li = document.querySelector("li:last-of-type");
 li.textContent = li.textContent + " (Changed!)";
 
+const body = document.body;
 
-
-// const listItemElements = document.querySelectorAll('li');
-const listItemElements = document.getElementsByTagName('li');
+// const listItemElements = document.querySelectorAll('li');//it only takes a snapshot of the data at the begining
+const listItemElements = document.getElementsByTagName('li');//this is updated in realtime
 
 for (const listItemEl of listItemElements) {
   console.dir(listItemEl);
